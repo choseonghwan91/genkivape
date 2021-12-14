f3700d38644e1fc8dba615db9043b91132a174d0
EUi SEON CHI <bbcd6222@gmail.com>
2021-12-15 00:35:41 +0900

    [Add] footer basic setup

--- src/RouterFile.tsx
+++ src/RouterFile.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/footer/Footer";
 import Home from "./pages/Home/Home";
 import Location from "./pages/Location/Location";
 import Ourstory from "./pages/Ourstory/Ourstory";
@@ -12,6 +13,7 @@ const RouterFile = function () {
         <Route path="/location" element={<Location />} />
         <Route path="/ourstory" element={<Ourstory />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/footer" element={<Footer />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div>
+      <footer className=" flex flex-row justify-center">
+        <div>
+          <div className="">
+            <div className="">
+              <img
+                className="w-[150px] h-[200px]"
+                src="https://www.genkiyaki.com/wp-content/uploads/2021/09/genkiyaki-logo.png"
+              ></img>
+            </div>
+            <div className="flex justify-around">facebook instagram</div>
+            <div className="">
+              <ul className="flex justify-around">
+                <li>Our Story</li>
+                <li>Products</li>
+                <li>Location</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+            <div className="flex justify-center">
+              <p>©2021 Genkivape. All Rights Reserved.</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
